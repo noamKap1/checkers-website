75960fb8977e1a838000804ff2cd4d240bbecdb8
unknown <noamnhksd@gmail.com>
2023-11-23 20:02:55 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Login from "./auth/Login";
+import AdsComponent from './AdsComponent';
 
 function App() {
   return (
     <div className="App">
+        <AdsComponent dataAdSlot='8146813325679288' />
         <Login></Login>
     </div>
   );
